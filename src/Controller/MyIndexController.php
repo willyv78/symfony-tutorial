2f53c7a1e906a081896a9cee8c976327db7aefa4
Wilson Velandia <willyv78@gmail.com>
2020-09-17 16:02:49 -0500

    Se crea el primer controlador

---
+++ src/Controller/MyIndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyIndexController
+{
+    /**
+     * @Route("/")
+     */
+    public function home()
+    {
+        return new Response(
+            '<html><body>¡Hola mundo Symfony 5!</body></html>'
+        );
+    }
+}
